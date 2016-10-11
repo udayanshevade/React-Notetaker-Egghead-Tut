436fff318bc066aed318444f0eba8b587334211b
Udayan Shevade <udayan.shevade@gmail.com>
2016-10-11 17:52:36 -0400

    feat: Initialize Firebace + ReactFire

--- app/App.js
+++ app/App.js
@@ -1,9 +1,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Router = require('react-router').Router;
+var Firebase = require('firebase');
 var routes = require('./config/routes');
 
 import { hashHistory } from 'react-router';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBpBxfNCtzsS9fC5lit7PtmkzGWjRyJOJY",
+  authDomain: "first-react-app-a76ec.firebaseapp.com",
+  databaseURL: "https://first-react-app-a76ec.firebaseio.com",
+  storageBucket: "first-react-app-a76ec.appspot.com",
+  messagingSenderId: "763887315114"
+};
+Firebase.initializeApp(config);
 
 ReactDOM.render(
   <Router history={hashHistory}>{routes}</Router>,
